0b52d2d79d1d7668028cb45d15cf9e0e71999ed0
Igor <apofiosis2@gmail.com                                                                                git config --global user.name Igor>
2025-05-03 14:56:15 +0300

    checkbox1

--- chekbox/chekbox1.py
+++ chekbox/chekbox1.py
@@ -7,18 +7,29 @@ from selenium.webdriver.chrome.service import Service
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome( service=service)
 
-driver.get("https://the-internet.herokuapp.com/checkboxes")
+# driver.get("https://the-internet.herokuapp.com/checkboxes")
+# time.sleep(2)
+# checkbox = driver.find_elements("xpath","//input[@type='checkbox']")
+# checkbox[0].click()
+# print ("Первый чекбокс выбран:", checkbox[0].is_selected())
+#
+# assert checkbox[0].is_selected() == True
+# time.sleep(2)
+#
+# checkbox1 = driver.find_elements("xpath","//input[@type='checkbox']")
+#
+# checkbox[1].click()
+# print("второй чек бокс", checkbox[1].is_selected())  # is_selected определяем тип элемента
+# assert checkbox[1].is_selected() == False
+# time.sleep(2)
+
+driver.get("https://demoqa.com/checkbox")
+# Элемент для клика, чтобы выставить флажок
+checkbox = driver.find_element("xpath","//span[@class='rct-checkbox']")
+# Сам чек-бокс для проверки статуса
+checkbox1 = driver.find_element("xpath","//span[@class='rct-node-icon']")
 time.sleep(2)
-checkbox = driver.find_elements("xpath","//input[@type='checkbox']")
-checkbox[0].click()
-print ("Первый чекбокс выбран:", checkbox[0].is_selected())
 
-assert checkbox[0].is_selected() == True
-time.sleep(2)
+checkbox1.click()
 
-checkbox1 = driver.find_elements("xpath","//input[@type='checkbox']")
-
-checkbox[1].click()
-print("второй чек бокс", checkbox[1].is_selected())  # is_selected определяем тип элемента
-assert checkbox[1].is_selected() == False
 time.sleep(2)
